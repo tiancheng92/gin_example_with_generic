e211517d0e238e56174eafd2070c15ab80e00a3c
徐天成 <tiancheng.xu@yo-star.com>
2022-06-14 17:39:23 +0800

    feat: 简化error code

--- pkg/ecode/ecode.go
+++ pkg/ecode/ecode.go
@@ -4,59 +4,31 @@ import (
 	"gin_example_with_generic/pkg/errors"
 )
 
-// ErrCode implements errors.Coder interface.
 type ErrCode struct {
-	// C refers to the code of the ErrCode.
-	C int
-	// HTTP status that should be used for the associated error code.
-	HTTP int
-	// External (user) facing error text.
-	Ext string
-	// Ref specify the reference document.
-	Ref string
+	ErrCode    int
+	HttpStatus int
+	Message    string
 }
 
-var _ errors.Coder = &ErrCode{}
-
-// Code returns the integer code of ErrCode.
 func (coder ErrCode) Code() int {
-	return coder.C
+	return coder.ErrCode
 }
 
-// String implements stringer. String returns the external error message,
-// if any.
 func (coder ErrCode) String() string {
-	return coder.Ext
+	return coder.Message
 }
 
-// Reference returns the reference document.
-func (coder ErrCode) Reference() string {
-	return coder.Ref
-}
-
-// HTTPStatus returns the associated HTTP status code, if any. Otherwise,
-// returns 200.
 func (coder ErrCode) HTTPStatus() int {
-	if coder.HTTP == 0 {
+	if coder.HttpStatus == 0 {
 		return 500
 	}
-
-	return coder.HTTP
+	return coder.HttpStatus
 }
 
-// nolint: unparam,deadcode
-func register(code int, httpStatus int, message string, refs ...string) {
-	var reference string
-	if len(refs) > 0 {
-		reference = refs[0]
-	}
-
-	coder := &ErrCode{
-		C:    code,
-		HTTP: httpStatus,
-		Ext:  message,
-		Ref:  reference,
-	}
-
-	errors.MustRegister(coder)
+func register(code int, httpStatus int, message string) {
+	errors.MustRegister(&ErrCode{
+		ErrCode:    code,
+		HttpStatus: httpStatus,
+		Message:    message,
+	})
 }
