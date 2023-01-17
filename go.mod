bd77075d2c807e58c3f81e159121550582553e0f
tiancheng.xu <kiritoeva@icloud.com>
2023-01-17 18:05:04 +0800

    update

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module gin_example_with_generic
 go 1.18
 
 require (
+	github.com/fsnotify/fsnotify v1.5.1
 	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-contrib/pprof v1.3.0
 	github.com/gin-gonic/gin v1.8.1
@@ -25,7 +26,6 @@ require (
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
-	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
--- store/model/country.go
+++ store/model/country.go
@@ -2,7 +2,16 @@ package model
 
 import (
 	"gin_example_with_generic/generic"
+	"gorm.io/gorm"
 )
+
+var countryWatcher []chan Country
+
+func RegisterCountryWatcher() chan Country {
+	var channel = make(chan Country)
+	countryWatcher = append(countryWatcher, channel)
+	return channel
+}
 
 type Country struct {
 	generic.Model `yaml:",inline"`
@@ -13,4 +22,11 @@ type Country struct {
 
 func (Country) GetFuzzySearchFieldList() []string {
 	return []string{"name", "name_cn", "short_name"}
+}
+
+func (c Country) AfterUpdate(_ *gorm.DB) error {
+	for _, v := range countryWatcher {
+		v <- c
+	}
+	return nil
 }
